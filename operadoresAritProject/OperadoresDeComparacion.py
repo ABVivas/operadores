365c7a9ac1e4e4d541cb6d80deda14229b20cc02
Angie Vivas <angievivas1107@gmail.com>
2024-09-26 16:48:38 -0500

    se crea el proyecto con los ejemplos de los operadores

---
+++ operadoresAritProject/OperadoresDeComparacion.py
@@ -0,0 +1,11 @@
+#ejemplo operadores de comparacion
+m = int(input("ingrese el valor de m "))
+n = int(input("ingrese el valor de n "))
+
+print(f"la igualdad es: {(m == n)}") #igualdad
+print(m > n) #mayor que
+print(m < n) #menor que
+print(m >= n) #mayor o igual que
+print(m <= n) #menos o igual que
+print(m != n) #no igual
+
---
+++ operadoresAritProject/OperadoresLogicos.py
@@ -0,0 +1,10 @@
+#ejemplo operadores logicos
+#and
+q = 5
+print(q > 4 and q < 9)
+
+#or
+print(q > 5 or q <10)
+
+#not
+print(not(q > 2 and q < 7))
